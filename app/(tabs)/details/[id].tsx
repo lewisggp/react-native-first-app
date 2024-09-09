cbd5a189ac0b6b13141561ce267ca78a77b7a554
lewisggp <lewisggp@users.noreply.github.com>
2024-09-08 20:42:45 -0400

    upgrade detail route ui

--- app/(tabs)/details/[id].tsx
+++ app/(tabs)/details/[id].tsx
@@ -1,6 +1,14 @@
-import { Image, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+
+import { ActivityIndicator, Image, StyleSheet } from "react-native";
 
 import { Link, useLocalSearchParams } from "expo-router";
+
+import { useThemeColor } from "@/hooks/useThemeColor";
+
+import { getGameDetails } from "@/lib/metacritic";
+
+import type { GameType } from "@/types/GameType";
 
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
@@ -8,13 +16,31 @@ import { ThemedView } from "@/components/ThemedView";
 
 export default function Detail() {
     const { id } = useLocalSearchParams();
+    const [game, setGame] = useState<GameType | undefined>(undefined);
+    const color = useThemeColor({}, 'text');
+
+    useEffect(() => {
+        if (id) {
+            getGameDetails(id).then(setGame);
+        }
+    }, [id])
+
+    if (!game) {
+        return (
+            <ThemedView
+                className='flex-1 justify-center items-center'
+            >
+                <ActivityIndicator color={color} size={'large'} />
+            </ThemedView>
+        );
+    }
 
     return (
         <ParallaxScrollView
             headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
             headerImage={
                 <Image
-                    source={require('@/assets/images/partial-react-logo.png')}
+                    source={{ uri: game.image }}
                     style={styles.gameImage}
                 />
             }>
@@ -22,26 +48,29 @@ export default function Detail() {
                 className='flex-1 justify-center items-center'
             >
                 <ThemedText
-                    className='font-blod mb-8 text-2xl'
+                    className='font-bold mb-8 text-2xl'
                 >
-                    Detalle del juego {id}
+                    {game.title}
+                </ThemedText>
+                <ThemedText
+                    className='mb-4 text-lg'
+                >
+                    {game.description}
                 </ThemedText>
                 <Link href='/' className='text-blue-500'>
                     Volver atrás
                 </Link>
             </ThemedView>
         </ParallaxScrollView>
-
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     gameImage: {
-        height: 178,
-        width: 290,
-        bottom: 0,
-        left: 0,
+        height: 250,
+        width: '100%',
         position: 'absolute',
+        resizeMode: 'cover',
     },
     titleContainer: {
         flexDirection: 'row',
